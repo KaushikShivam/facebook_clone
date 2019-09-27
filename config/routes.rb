58e6a1fe26fa957e96cbb7a936daccd375c9a904
fatima ahmed <fatymah001@gmail.com>
2019-09-27 20:04:11 +0300

    Configure custom routes for user login,logout and edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_up: 'signup', sign_in: 'login', edit: 'edit-user'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'posts#index'
 end
