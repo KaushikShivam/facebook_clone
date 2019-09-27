b0213a2500f790e6732c3f2c7a9802ea99dbe29f
fatima ahmed <fatymah001@gmail.com>
2019-09-27 23:50:40 +0300

    Implemented integration test for user sign up

---
+++ spec/features/user_login_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'user login', type: :feature do
+    scenario 'signs up the user correctly' do
+      visit new_user_registration_path
+      fill_in('user[name]', with: 'Example')
+      fill_in('user[email]', with: 'example@email.com')
+      fill_in('user[password]', with: 'foobar')
+      fill_in('user[password_confirmation]', with: 'foobar')
+      fill_in('user[image_link]', with: 'foobar')
+      click_button('Sign up')
+      expect(page).to have_content('Welcome! You have signed up successfully')
+    end
+  end
+
+
+
+#   visit new_user_session_path
+#   fill_in('Email', with: 'example@email.com')
+#   fill_in('Password', with: 'foobar')
+#   click_button('Log in')
