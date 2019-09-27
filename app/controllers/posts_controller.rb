aa831121753ce500f06de0882c03ec3412701c9e
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-09-27 23:10:05 +0530

    Add post creation feature by user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,18 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
   
   def new
+    @post = Post.new
   end
   
   def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to root_path
+      flash[:notice] = "Post created successfully"
+    else
+      render 'new'
+      flash.now[:alert] = "Post creation failed. Please enter valid information"
+    end
   end
   
   def edit
@@ -18,6 +27,15 @@ class PostsController < ApplicationController
   end
   
   def index
+  end
+  
+  private
+  def post_params
+    params.require(:post).permit(:content, :image_link)
+  end
+  
+  def get_post
+    @post = Post.find(params[:id])
   end
   
 end
