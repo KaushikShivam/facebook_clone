442e1587aa860d0288aff06cf181d56d66f7cebc
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-09-27 22:58:30 +0530

    Add auth for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,22 @@
 class PostsController < ApplicationController
   
+  before_action :authenticate_user!
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
   def index
   end
   
