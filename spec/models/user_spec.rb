21bc029f2bbb0014468ad97df3a44fb5ac0c8a88
fatima ahmed <fatymah001@gmail.com>
2019-09-27 23:22:23 +0300

    Implement unit tests for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = User.new(name: "fatima", password: "password",password_confirmation: "password", email: "fatima@gmail.com", image_link: "jfiehbn")
+  end
+  context 'valid user' do
+    it 'has a valid name' do
+      expect(@user).to be_valid
+    end
+    it 'has a valid image link' do
+      expect(@user).to be_valid
+    end
+  end
+  context "invalid user" do
+    it 'has an invalid name' do
+      @user.name=" "
+      expect(@user).to_not be_valid
+    end
+    it 'has an invalid image link' do
+      @user.image_link=" "
+    end
+  end
 end
