7bfd268c1fcc8923e716e3df8a2d49e448f16edd
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-09-30 14:34:07 +0530

    Fix user redundant login message

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,15 @@ Rails.application.routes.draw do
   resources :posts
   devise_for :users, path: '', path_names: { sign_up: 'signup', sign_in: 'login', edit: 'edit-user'}
   
-  # authenticated :user do
-  #   root to: 'posts#index'
-  # end
-  
-  
   get 'users/show/:id', to: 'users#show'
   get 'users', to: 'users#index'
+  
+  authenticated :user do
+    root to: 'posts#index'
+  end
   
   devise_scope :user do
     root to: 'devise/sessions#new'
   end
-  authenticated :user do
-    root to: 'posts#index'
-  end
+  
 end
