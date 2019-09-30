ab0986873132650f5198118ed7eef2382f0757d9
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-09-30 13:50:14 +0530

    Add users controller with index and show methods

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :posts
   devise_for :users, path: '', path_names: { sign_up: 'signup', sign_in: 'login', edit: 'edit-user'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/show/:id', to: 'users#show'
+  get 'users', to: 'users#index'
+  
   root to: 'posts#index'
 end
