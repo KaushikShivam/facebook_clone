d9fb2488297928f489185f41b5cf35e4babd85c3
fatima ahmed <fatymah001@gmail.com>
2019-09-30 23:42:32 +0300

    Add integration test for another user liking a post

--- spec/features/user_likes_post_spec.rb
+++ spec/features/user_likes_post_spec.rb
@@ -16,8 +16,23 @@ RSpec.describe 'user likes a post', type: :feature do
     visit post_path(@post)
   end
   
-  scenario "successfully" do
+  scenario "creater of post likes successfully" do
     click_on("Like")    
+    expect(page).to have_content('1 likes')
+  end
+
+  scenario "another user likes a post successfully" do
+    @another_user=User.create(name: 'another',email: 'another@user.com', image_link: 'dvbafa',
+                              password: 'password', password_confirmation: 'password')
+    click_on('Log Out')
+    expect(page).to have_content('Signed out successfully.')
+    visit new_user_session_path
+    fill_in('user[email]', with: @another_user.email)
+    fill_in('user[password]', with: @another_user.password)
+    click_button('Log in')
+    visit post_path(@post)
+    click_on("Like")
+    expect(page).to have_content('Post liked successfully')
     expect(page).to have_content('1 likes')
   end
 end
