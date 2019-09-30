9e95fdd2b9037014620debb5497239e20488fe18
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-09-30 14:18:18 +0530

    TODO: Fix root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
   resources :posts
   devise_for :users, path: '', path_names: { sign_up: 'signup', sign_in: 'login', edit: 'edit-user'}
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # authenticated :user do
+  #   root to: 'posts#index'
+  # end
+  
+  
   get 'users/show/:id', to: 'users#show'
   get 'users', to: 'users#index'
   
-  root to: 'posts#index'
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
+  authenticated :user do
+    root to: 'posts#index'
+  end
 end
