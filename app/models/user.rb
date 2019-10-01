30df914aa6560a82a002d0debd66281f9842127c
fatima ahmed <fatymah001@gmail.com>
2019-10-01 20:45:14 +0300

    Implement enums for friendship status

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ApplicationRecord
   has_many :friendships
   has_many :friends, through: :friendships
   
+  
   def self.all_except(user)
     where.not(id: user)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
     resources :comments
     resources :likes
   end
-
-
-
+  
   devise_for :users, path: '', path_names: { sign_up: 'signup', sign_in: 'login', edit: 'edit-user'}
   
   get 'users/show/:id', to: 'users#show'
