1a3806e9dd0af311dbf4184422bf030c013e564f
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-10-03 00:44:36 +0530

    Add unit tests for the friendship

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = User.create!(name: "user", email: "email@email.com", password: "password", password_confirmation: "password")
+    @friend = User.create!(name: "friend", email: "friend@email.com", password: "password", password_confirmation: "password")
+    @Friendship = Friendship.new(user: @user, friend: @friend, status: 1)
+  end
+  
+  context "Friendship" do
+    it "with valid user" do
+      expect(@Friendship).to be_valid
+    end
+    
+    it "with invalid user" do
+      @Friendship.user = nil
+      expect(@Friendship).to_not be_valid
+    end
+    
+    it "with invalid friend" do
+      @Friendship.friend = nil
+      expect(@Friendship).to_not be_valid
+    end
+    
+  end
+  
 end
