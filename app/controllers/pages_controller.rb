4d812c4910cb93d283519165ef483ac29e385a1d
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-10-04 01:58:37 +0530

    Add styling to the suggested users section

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   
   def home
+    @suggested_people = (User.all_except(current_user) - current_user.friends).first(10)
+    
   end
   
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,12 @@
 module PagesHelper
+  def gravatar_helper user
+    image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", class: 'rounded-circle', width: 50
+  end
+  
+  def random_image
+    random_id = rand(100)
+    "https://picsum.photos/id/#{random_id}/400/200"
+  end
+  
+  
 end
