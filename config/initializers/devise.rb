792e3b26f23f58ba7531711dc619e17d9dca56d2
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-10-04 02:33:23 +0530

    Replace fb test keys instead of using ENV file

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -297,6 +297,6 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
   
-  config.omniauth :facebook, ENV.fetch("TEST_APP_ID"), ENV.fetch("TEST_SECRET"), callback_url: "http://localhost:3000/auth/facebook/callback"
+  config.omniauth :facebook, "421967731794807", "a63deac6505c21e6844d337eedb0ae49", callback_url: "http://localhost:3000/auth/facebook/callback"
   
 end
