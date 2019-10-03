d615ec4e63a091c8dc3f23c57706c6747e326d7e
fatima ahmed <fatymah001@gmail.com>
2019-10-04 01:16:47 +0300

    Add styling for friends section and create new post in news feed page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   
   def home
     @suggested_people = (User.all_except(current_user) - current_user.friends).first(10)
-    
+    @friends = current_user.friends
+    @new_post = Post.new
   end
-  
 end
